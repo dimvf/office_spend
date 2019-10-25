0063b9237a4ff7579d226c4e780f887e50d77d39
dimvf <dima.volosatov@gmail.com>
2019-10-26 01:22:01 +0300

    new

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from dialog_bot_sdk.bot import DialogBot
 import grpc
+from flask import Flask
+
+app = Flask(__name__)
 
 def on_msg(*params):
         bot.messaging.send_message(peer, "Введите 'help' для помощи")
 
-if __name__ == '__main__':
+
+@app.route('/')
+def main():
     bot = DialogBot.get_secure_bot(
         "hackathon-mob.transmit.im",  # bot endpoint from environment
         grpc.ssl_channel_credentials(), # SSL credentials (empty by default!)
