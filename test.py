af682ccafec788aee4fb14f58f05e832a2a58840
dimvf <dima.volosatov@gmail.com>
2019-10-26 01:25:30 +0300

    new

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ def on_msg(*params):
 
 @app.route('/')
 def main():
-    bot = DialogBot.get_secure_bot(
-        "hackathon-mob.transmit.im",  # bot endpoint from environment
-        grpc.ssl_channel_credentials(), # SSL credentials (empty by default!)
-        '7f38e457bebadf578dbba6d5ccbacd4b3e926938'  # bot token from environment
-    )
-    bot.messaging.on_message(on_msg)
+    return "Hello world"
+    #bot = DialogBot.get_secure_bot(
+    #    "hackathon-mob.transmit.im",  # bot endpoint from environment
+    #    grpc.ssl_channel_credentials(), # SSL credentials (empty by default!)
+    #    '7f38e457bebadf578dbba6d5ccbacd4b3e926938'  # bot token from environment
+    #)
+    #bot.messaging.on_message(on_msg)
